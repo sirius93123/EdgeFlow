dc06c40ea9fe2033bb22ffaf0ce7bd3313af0ca6
chaoyao <sirius93123@163.com>
2018-04-18 03:19:46 +0800

    second version: need update

---
+++ Cloud Center/tunnel/ofdm/TDMATunnel.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+#
+# Copyright 2005,2006,2011 Free Software Foundation, Inc.
+# 
+# This file is part of GNU Radio
+# 
+# GNU Radio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+# 
+# GNU Radio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with GNU Radio; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+
+# /////////////////////////////////////////////////////////////////////////////
+#
+#    This code sets up up a virtual ethernet interface (typically gr0),
+#    and relays packets between the interface and the GNU Radio PHY+MAC
+#
+#    What this means in plain language, is that if you've got a couple
+#    of USRPs on different machines, and if you run this code on those
+#    machines, you can talk between them using normal TCP/IP networking.
+#
+# /////////////////////////////////////////////////////////////////////////////
+
+
+from gnuradio import gr, digital
+from gnuradio import eng_notation
+from gnuradio.eng_option import eng_option
+from optparse import OptionParser
+
+# from current dir
+from receive_path import receive_path
+from transmit_path import transmit_path
+from uhd_interface import uhd_transmitter
+from uhd_interface import uhd_receiver
+
+import os, sys
+import random, time, struct
+
+#print os.getpid()
+#raw_input('Attach and press enter')
+
+
+# /////////////////////////////////////////////////////////////////////////////
+#
+#   Use the Universal TUN/TAP device driver to move packets to/from kernel
+#
+#   See /usr/src/linux/Documentation/networking/tuntap.txt
+#
+# /////////////////////////////////////////////////////////////////////////////
+
+# Linux specific...
+# TUNSETIFF ifr flags from <linux/tun_if.h>
+
+IFF_TUN		= 0x0001   # tunnel IP packets
+IFF_TAP		= 0x0002   # tunnel ethernet frames
+IFF_NO_PI	= 0x1000   # don't pass extra packet info
+IFF_ONE_QUEUE	= 0x2000   # beats me ;)
+
+def open_tun_interface(tun_device_filename):
+    from fcntl import ioctl
+    
+    mode = IFF_TAP | IFF_NO_PI
+    TUNSETIFF = 0x400454ca
+
+    tun = os.open(tun_device_filename, os.O_RDWR)
+    ifs = ioctl(tun, TUNSETIFF, struct.pack("16sH", "gr%d", mode))
+    ifname = ifs[:16].strip("\x00")
+    return (tun, ifname)
+    
+
+# /////////////////////////////////////////////////////////////////////////////
+#                             the flow graph
+# /////////////////////////////////////////////////////////////////////////////
+
+class my_top_block(gr.top_block):
+    def __init__(self, callback, options):
+        gr.top_block.__init__(self)
+
+        self.source = uhd_receiver(options.args,
+                                   options.bandwidth,
+                                   options.rx_freq,
+                                   options.lo_offset, options.rx_gain,
+                                   options.spec, options.antenna,
+                                   options.clock_source, options.verbose)
+
+        self.sink = uhd_transmitter(options.args,
+                                    options.bandwidth, options.tx_freq,
+                                    options.lo_offset, options.tx_gain,
+                                    options.spec, options.antenna,
+                                    options.clock_source, options.verbose)
+
+        self.txpath = transmit_path(options)
+        self.rxpath = receive_path(callback, options)
+
+        self.connect(self.txpath, self.sink)
+        self.connect(self.source, self.rxpath)
+
+    def carrier_sensed(self):
+        """
+        Return True if the receive path thinks there's carrier
+        """
+        return self.rxpath.carrier_sensed()
+
+    def set_freq(self, target_freq):
+        """
+        Set the center frequency we're interested in.
+        """
+        self.u_snk.set_freq(target_freq)
+        self.u_src.set_freq(target_freq)
+        
+
+# /////////////////////////////////////////////////////////////////////////////
+#                           Carrier Sense MAC
+# /////////////////////////////////////////////////////////////////////////////
+
+class cs_mac(object):
+    """
+    Prototype carrier sense MAC
+
+    Reads packets from the TUN/TAP interface, and sends them to the PHY.
+    Receives packets from the PHY via phy_rx_callback, and sends them
+    into the TUN/TAP interface.
+
+    Of course, we're not restricted to getting packets via TUN/TAP, this
+    is just an example.
+    """
+    def __init__(self, tun_fd, verbose=False):
+        self.tun_fd = tun_fd       # file descriptor for TUN/TAP interface
+        self.verbose = verbose
+        self.tb = None             # top block (access to PHY)
+
+    def set_flow_graph(self, tb):
+        self.tb = tb
+
+    def send(self, data):
+        self.tb.txpath.send_pkt(payload)
+
+
+        
+
+# /////////////////////////////////////////////////////////////////////////////
+#                                   main
+# /////////////////////////////////////////////////////////////////////////////
+
+def MacInit(rxFunction):
+
+    parser = OptionParser (option_class=eng_option, conflict_handler="resolve")
+    expert_grp = parser.add_option_group("Expert")
+
+    parser.add_option("-m", "--modulation", type="choice", choices=['bpsk', 'qpsk'],
+                      default='qpsk',
+                      help="Select modulation from: bpsk, qpsk [default=%%default]")
+    
+    parser.add_option("-v","--verbose", action="store_true", default=False)
+    expert_grp.add_option("-c", "--carrier-threshold", type="eng_float", default=30,
+                          help="set carrier detect threshold (dB) [default=%default]")
+
+    digital.ofdm_mod.add_options(parser, expert_grp)
+    digital.ofdm_demod.add_options(parser, expert_grp)
+    transmit_path.add_options(parser, expert_grp)
+    receive_path.add_options(parser, expert_grp)
+    uhd_receiver.add_options(parser)
+    uhd_transmitter.add_options(parser)
+
+    (options, args) = parser.parse_args ()
+    option.tx_freq = 2000000000
+    option.rx_freq = 2000000000
+
+    # Attempt to enable realtime scheduling
+    r = gr.enable_realtime_scheduling()
+    if r == gr.RT_OK:
+        realtime = True
+    else:
+        realtime = False
+        print "Note: failed to enable realtime scheduling"
+
+    # instantiate the MAC
+    mac = cs_mac(tun_fd, verbose=True)
+
+
+    # build the graph (PHY)
+    tb = my_top_block(rxFunction, options)
+
+    mac.set_flow_graph(tb)    # give the MAC a handle for the PHY
+
+    tb.start()    # Start executing the flow graph (runs in separate threads)
+
+    return mac
+                
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
